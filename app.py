9c00784b3743b840709d107cc361ee9c2025e7a8
Riley Cox <Riley.cox112@gmail.com>
2020-06-25 10:27:59 +1200

    added template

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 @app.errorhandler(404)
 def error404(e):
